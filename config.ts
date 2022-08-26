3cf3e3ffe9dd67877a3553e28f8fb0aecbca88ab
R V <2096tester@gmail.com>
2022-08-25 18:07:21 -0700

    .ignore

--- config.ts
+++
@@ -1,9 +0,0 @@
-import { config as loadEnv } from "https://deno.land/x/dotenv/mod.ts";
-/*const configData = await loadEnv({
-  export: true,
-  allowEmptyValues: true,
-});*/
-
-export const config = loadEnv();
-console.log(config.GITHUB_CLIENT_ID);
-console.log(`GITHUB_CLIENT_ID=${Deno.env.get("GITHUB_CLIENT_ID")}`);
